af090543421d221bfe9778929f192175ca4f4f01
parsa pirhadi <parsapirhadi.98@gmail.com>
2021-08-02 15:29:17 +0430

    android git+nav ++

---
+++ app/src/main/java/frg.kt
@@ -0,0 +1,11 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class frg: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
